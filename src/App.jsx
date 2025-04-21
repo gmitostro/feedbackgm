67646ad64aef099d5193a1bbdb572eb62c363584
guimilbeyer <guimilbeyer@gmail.com>
2025-04-21 22:38:34 +0000

    fim

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from "react"
 import { CardInicial } from "./components/CardInicial"
 import { CardFinal } from "./components/CardFinal"
 
-
 export function App() {
   const [notaAvaliacao, setNotaAvaliacao] = useState(0)
   const [submited, setSubmited] = useState(false)
 
 return (
   submited === false ? (
-    <Cardinicial setNotaAvaliacao={setNotaAvaliacao} notaAvaliacao={notaAvaliacao} setSubmited={setSubmited} />
+    <CardInicial setNotaAvaliacao={setNotaAvaliacao} notaAvaliacao={notaAvaliacao} setSubmited={setSubmited} />
     ) : (
       <CardFinal notaAvaliacao={notaAvaliacao} />
     )
